24050ac707d9d356f575cadbfa6190c973984076
alessandro petocchi <a.petocchi@icloud.com>
2019-07-29 14:42:51 +0000

    header file

---
+++ sendAck.h
@@ -0,0 +1,36 @@
+/**
+ *  @author Alessandro Petocchi, Giuseppe Leone
+ */
+
+#ifndef SENDACK_H
+#define SENDACK_H
+
+//payload of the msg
+typedef nx_struct my_msg1 {
+	nx_uint8_t msg_type; 
+	nx_uint16_t msg_id;
+} truckMsg;
+
+typedef nx_struct my_msg2 {
+	nx_uint8_t msg_type; 
+	nx_uint16_t msg_id;
+	nx_uint16_t excess_trash;
+} moveMsg;
+
+typedef nx_struct my_msg3 {
+	nx_uint8_t msg_type; 
+	nx_uint16_t msg_id;
+	nx_uint16_t coord_X;
+	nx_uint16_t coord_Y;
+	nx_uint8_t node_ID;
+} alertMsg;
+
+#define ALERT 1
+#define MOVE 2 
+#define TRUCK 3
+
+enum{
+AM_MY_MSG = 6,
+};
+
+#endif
